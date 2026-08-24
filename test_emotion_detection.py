aa4b3c5268c55c75ddf6c315a25e98e11a80880d
vllevinton <valentinelio04@gmail.com>
2026-08-24 17:52:57 -0300

    Update import statement for emotion_detector

--- test_emotion_detection.py
+++ test_emotion_detection.py
@@ -1,6 +1,6 @@
 """Unit tests for the EmotionDetection package."""
 import unittest
-from EmotionDetection import emotion_detector
+from EmotionDetection.emotion_detection import emotion_detector
 
 
 class TestEmotionDetector(unittest.TestCase):
