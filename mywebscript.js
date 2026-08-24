8e8fed9b6eb4e783c79d6618decde78784be6d71
vllevinton <valentinelio04@gmail.com>
2026-08-24 17:47:01 -0300

    Delete mywebscript.js

--- mywebscript.js
+++
@@ -1,8 +0,0 @@
-async function runEmotionDetection() {
-  const text = document.getElementById("textToAnalyze").value;
-  const result = document.getElementById("result");
-  const response = await fetch(
-    "/emotionDetector?textToAnalyze=" + encodeURIComponent(text)
-  );
-  result.textContent = await response.text();
-}
